3cff40b0ee89d176530f59bdf0effa1c61be0b36
Jahongir Turgunboyev <j.turgunboyev@technocorp.uz>
2023-06-13 12:47:09 +0500

    more changes

--- app/task_project/urls.py
+++ app/task_project/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from .views import ProjectViewSet, TaskViewSet
 
 router = routers.DefaultRouter()
-router.register(r'task', TaskViewSet.as_view(), basename="task")
-router.register(r'project', ProjectViewSet.as_view(), basename="task")
+router.register(r'task', TaskViewSet, basename="task")
+router.register(r'project', ProjectViewSet, basename="task")
 
 urlpatterns = router.urls
--- app/task_project/views.py
+++ app/task_project/views.py
@@ -1,14 +1,31 @@
 from rest_framework import viewsets, mixins
 from .models import Project, Task
 from .serializers import ProjectSerializer, TaskSerializer
+from rest_framework.response import Response
 
 
-class ProjectViewSet(viewsets.ViewSet):
+class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
 
 
-class TaskViewSet(viewsets.ViewSet):
+class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        # Update the associated Task here
+        task_instance = instance.task
+        if task_instance:
+            if instance.status == "To'landi":
+                task_instance.status = True
+                task_instance.save()
+
+        return Response(serializer.data)
 
